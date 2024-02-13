f742f1830416324cb029c7c6965ebe53941e6413
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-13 13:06:24 -0300

    Agregados comentarios sobre el uso de CommonJS y ES6 en el archivo index.js

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Aplicacion corriendo en --->>>> http://localhost:${port}`);
 });
+
+//commonjs
+//exportar
+// modules.exports = app;
+//importar
+// const app = require("./src/app/app.js")
+
+
+// ES6
+// export default app;
+// import app from "./src/app/app.js";
