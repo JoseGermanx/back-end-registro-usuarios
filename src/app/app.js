be5a2ebf40a3b98a2e6fc9fc58532bf66612300a
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-13 12:52:30 -0300

    Agregadas configuraciones de middleware y ruta de error 404 en app.js

--- src/app/app.js
+++ src/app/app.js
@@ -6,8 +6,12 @@ const router = require("../routes/user.routes.js");
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 app.use("/api/v1", router);
 
-
+app.use("*", (req, res) => { res.status(404).send("Ruta no encontrada")});
 
 module.exports = app;
