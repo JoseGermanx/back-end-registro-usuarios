a0cc4d06c52eb72bbf6aac74ec5af825bd30af91
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-21 15:44:17 -0300

    Agregadas dependencias jsonwebtoken, buffer-equal-constant-time, ecdsa-sig-formatter y sus respectivas versiones.

---
+++ src/services/generar-jwt.js
@@ -0,0 +1,30 @@
+
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (idUser = "") => {
+ 
+    return new Promise((resolve, reject) => {
+        const payload = {idUser};
+        jwt.sign(
+            payload,
+            "wiojwdfkjsdklfjslkdfjlskdajfsklndviwekndvsoklwei09fkjds",
+            {
+                expiresIn: "4h"
+            },
+            (err, token) => {
+                if (err) {
+                  console.log(err);
+                  reject("No se pudo generar el token");
+                } else {
+                  resolve(token);
+                }
+            }
+        )
+    })
+}
+
+
+
+
+
+module.exports = generarJWT;
