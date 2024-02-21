5156c66e130519912a1a7964e378d49a46a02812
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-21 17:11:00 -0300

    Actualizar puerto a variable de entorno

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require("./src/app/app.js")
 const { dbConnection } = require("./src/database/conexion.js")
 
-const port = 3000;
+const port = process.env.DEV_PORT;
 
 
 app.listen(port, () => {
